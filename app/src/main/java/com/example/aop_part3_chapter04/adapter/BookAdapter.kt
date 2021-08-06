39fe770f8d04ea2e439411d7fcbfc8f75c8303e7
20160553 <53904156+20160553@users.noreply.github.com>
2021-08-06 14:21:16 +0900

    리사이클러 뷰 아이템 확장 및 url 이미지 뷰 연결하기 위해 Glide 라이브러리 사용
    이미지 url이 http이기 때문에 http 허용.

--- app/src/main/java/com/example/aop_part3_chapter04/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/aop_part3_chapter04/adapter/BookAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.aop_part3_chapter04.databinding.ItemBookBinding
 import com.example.aop_part3_chapter04.model.Book
 
@@ -14,9 +15,15 @@ class BookAdapter: ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
         private val binding: ItemBookBinding
         ): RecyclerView.ViewHolder(binding.root) {
 
-
             fun bind(bookModel: Book) {
                 binding.titleTextView.text = bookModel.title
+                binding.descriptionTextView.text = bookModel.description
+
+                //이미지 로드 라이브러리 Glide를 통해 url로 이미지 뷰 연결
+                Glide
+                    .with(binding.coverImageView.context)
+                    .load(bookModel.coverSmallUrl)
+                    .into(binding.coverImageView)
             }
     }
 
